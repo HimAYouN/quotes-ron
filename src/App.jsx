41828d812653b990de161f1b6d5331222735f7f8
HimAYouN <himayoun7931@gmail.com>
2024-10-07 14:48:49 +0530

    bugs fixed, toasts added, added icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 
 import { useEffect, useState } from 'react'
+import { MdFavoriteBorder } from "react-icons/md";
+import { MdNavigateNext } from "react-icons/md";
+import { toast, ToastContainer } from 'react-toastify';
+
+
 import './App.css'
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [quote, setQuote] = useState();
@@ -11,7 +17,14 @@ function App() {
 
 /// Fetching the data ..
   const fetchData = async () => {
-    const response = await fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+    const response = await toast.promise(
+      fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes"),
+      {
+        pending: 'Finding Nemo',
+        success: 'Here we Go',
+        error: 'OPPS! something went wrong'
+      },{position: "top-center", autoClose:1000}
+  );
     const data = await response.json();
     setQuote(data[0]); 
   };
@@ -23,8 +36,13 @@ useEffect(() => {
 
 
 ///Adding to the favirote....
+useEffect(() => {
+  
+}, [favirotes])
 
 function addFavi(quote) {
+  toast.success("Added to favorites", { autoClose: 500 , position: "top-center"});
+
   // Retrieve the existing data from localStorage
   const existingFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
@@ -35,7 +53,8 @@ function addFavi(quote) {
   localStorage.setItem('favorites', JSON.stringify(existingFavorites));
 
   // console.log(`${quote} has been added to localStorage`);
-  allFavi()
+  const saved = JSON.parse(localStorage.getItem('favorites')) || [];
+  setFavirotes(saved)
 }
 
 
@@ -53,6 +72,7 @@ const allFavi = ()=>{
 
 //DELETING 
 const deleteFavorite = (index) => {
+  toast.success("Deleted!", {autoClose:500, position:'top-center'})
   const updatedFavorites = favirotes.filter((_, i) => i !== index);
   setFavirotes(updatedFavorites);
   localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
@@ -61,16 +81,15 @@ const deleteFavorite = (index) => {
 
   return (
     <>
-      <div className='shadow-md shadow-black rounded-lg p-10 '>
+      <div className='shadow-md shadow-black rounded-lg p-10 backdrop-blur-md'>
           <p>{quote}</p>
       </div>
       <div className='m-5 flex justify-center gap-7 '>
-        <button className='text-pink-400 shadow-sm shadow-pink-600  hover:text-pink-900 hover:shadow-pink-700 transition duration-300 ease-in-out'  onClick={()=>{addFavi(quote)}}>Favorite</button>
-        <button className='text-blue-400 shadow-sm shadow-blue-600 hover:text-blue-900 hover:shadow-blue-700 transition duration-300 ease-in-out' onClick={fetchData}>Next </button>
-
+        <button className='text-pink-400 shadow-sm shadow-pink-600  hover:text-pink-900 hover:shadow-pink-700 transition duration-300 ease-in-out'  onClick={()=>{addFavi(quote)}}><MdFavoriteBorder /></button>
+        <button className='text-blue-400 shadow-sm shadow-blue-600 hover:text-blue-900 hover:shadow-blue-700 transition duration-300 ease-in-out' onClick={fetchData}><MdNavigateNext /> </button>
       </div>
       <div className='mt-10'>
-        <button className='text-orange-400 shadow-sm shadow-orange-600 hover:text-orange-900 hover:shadow-orange-700 transition duration-300 ease-in-out' onClick={allFavi}>Favroties</button>
+        <button className='text-orange-400 shadow-sm shadow-orange-600 hover:text-orange-900 hover:shadow-orange-700 transition duration-300 ease-in-out' onClick={allFavi}>Favorites</button>
       </div>
       <div>
       { 
@@ -78,15 +97,16 @@ const deleteFavorite = (index) => {
     <div>
       { 
         favirotes.map((fav, index) => (
-          <p className='shadow-md shadow-black rounded-lg p-4 text-left' key={index}>
-            {fav}
+          <div className='shadow-md shadow-black rounded-lg p-4 text-left flex justify-between ' key={index}>
+            <p  > {fav}</p>
             <button onClick={()=>{deleteFavorite(index)}}>Delete</button>
-          </p>
+          </div>
         ))
       }
     </div>
   }
       </div>
+      <ToastContainer/>
     </>
   )
 }
